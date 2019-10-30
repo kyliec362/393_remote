9d370307222f4aca211626407a4e17a0b23c8564
Kylie Chesner <kyliec362@gmail.com>
2019-10-29 20:34:13 -0500

    added test file

---
+++ Deliverables/5/5.2/n_player/tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class PlayerTests(unittest.TestCase):
+
+    def test_sort(self):
+
+        self.assertEqual(b.sort(s), [0, 1, 3, 6, 8, 1000, "hi", "my", "wow", {"name": {"name": 3}}])
+
+
+if __name__ == "__main__":
+    unittest.main()
