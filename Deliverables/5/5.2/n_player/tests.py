2cc1e809045b8a2cadc007d6dc45756b7ebb188f
Rachel Anderson <rachelanderson2020@u.northwestern.edu>
2019-10-30 03:57:46 +0100

    add len()

--- Deliverables/5/5.2/n_player/tests.py
+++ Deliverables/5/5.2/n_player/tests.py
@@ -1,24 +1,39 @@
 import unittest
 from board import set_board_length
 from board import get_board_length
+from board import board
 from n_player import n_player
+
 
 class PlayerTests(unittest.TestCase):
 
+    def setUp(self):
+        self.n_player = n_player("B")
+        self.n_player.register_flag = True
+        self.n_player.receive_flag = True
+
     def test_make_capture_1_move(self):
-        self.assertEqual(...)
+        set_board_length(4)
+        board = [["W", "B", " ", " "],
+                 [" ", " ", " ", " "],
+                 [" ", " ", " ", " "],
+                 [" ", "B", "W", "B"]]
+        self.assertTrue(self.n_player.make_capture_1_move(board, self.n_player.stone, "1-2"))
+        self.assertFalse(self.n_player.make_capture_1_move(board, self.n_player.stone, "2-2"))
+        self.assertTrue(self.n_player.make_capture_1_move(board, self.n_player.stone, "3-3"))
+        self.assertFalse(self.n_player.make_capture_1_move(board, self.n_player.stone, "1-1"))
 
     def test_next_player_move(self):
         self.assertEqual(...)
 
-    def test_randomize_next_move(self):
-        self.assertEqual(...)
-
-    def test_make_capture_n_moves(self):
-        self.assertEqual(...)
-
-    def test_make_a_move(self):
-        self.assertEqual(...)
+    # def test_randomize_next_move(self):
+    #     self.assertEqual(...)
+    #
+    # def test_make_capture_n_moves(self):
+    #     self.assertEqual(...)
+    #
+    # def test_make_a_move(self):
+    #     self.assertEqual(...)
 
 
 if __name__ == "__main__":
