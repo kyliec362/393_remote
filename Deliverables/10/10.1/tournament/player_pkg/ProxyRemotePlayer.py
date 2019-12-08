bbd88075e1f97e7e503c348852578260754901ff
Kylie Chesner <kyliec362@gmail.com>
2019-12-08 13:07:12 -0600

    Update ProxyRemotePlayer.py

--- Deliverables/10/10.1/tournament/player_pkg/ProxyRemotePlayer.py
+++ Deliverables/10/10.1/tournament/player_pkg/ProxyRemotePlayer.py
@@ -60,12 +60,14 @@ class proxy_remote_player(Player):
             return False
 
     def register(self):
+        print("63 REGISTER !!!!!!!!!!!!!!")
         try:
             self.connection.sendall('["register"]'.encode())
             # TODO make sure we don't get crazy msg returned
             data = self.connection.recv(recv_size_player)
             print(67, data.decode())
             if data:
+                self.name = data.decode()
                 self.register_flag = True
                 return True
         except Exception as e:
