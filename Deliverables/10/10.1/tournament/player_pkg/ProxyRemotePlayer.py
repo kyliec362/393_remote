55217485482c97147af8bb161d2a8bf16825ae69
Kylie Chesner <kyliec362@gmail.com>
2019-12-12 21:08:30 -0600

    json dumps and encode messages

--- Deliverables/10/10.1/tournament/player_pkg/ProxyRemotePlayer.py
+++ Deliverables/10/10.1/tournament/player_pkg/ProxyRemotePlayer.py
@@ -15,23 +15,27 @@ class proxy_remote_player(Player):
 
     def make_a_move(self, boards):
         try:
-            move_msg = '["make-a-move",' + json.dumps(boards) + ']'
-            self.connection.sendall(move_msg.encode())
+            message = json.dumps(["make-a-move",' + json.dumps(boards) + ']).encode()
+            self.connection.sendall(message)
         except Exception as e:
             print("Make a move send -> Exception is %s" % e)
-            return False
+            # return False
+            self.connection.close()  # disconnect if failure
         try:
             data = self.connection.recv(recv_size_player)
             if data:
                 return data.decode()
-            return False
+            #return False
         except Exception as e:
             print("Make a move failed receiving. Exception is %s" % e)
-            return False
+            #return False
+            self.connection.close()  # disconnect if failure
+
 
     def register(self):
         try:
-            self.connection.sendall('["register"]'.encode())
+            message = json.dumps(["register"]).encode()
+            self.connection.sendall(message)
             data = self.connection.recv(recv_size_player)
             if data:
                 self.name = data.decode()
@@ -42,6 +46,7 @@ class proxy_remote_player(Player):
                 # return True
         except Exception as e:
             print("Register failed sending. Exception is %s" % e)
+            self.connection.close()  # disconnect if failure
             # return False
         #print("Register failed without an exception")
         # return False
@@ -49,10 +54,11 @@ class proxy_remote_player(Player):
 
     def receive_stones(self, stone):
         try:
-            recv_msg = '["receive-stones",' + '"' + stone + '"' + ']'
-            self.connection.sendall(recv_msg.encode())
+            message = json.dumps(["receive-stones", ' + '"' + stone + '"' + ']).encode()
+            self.connection.sendall(message)
         except Exception as e:
             print("Receive failed sending. Exception is %s" % e)
+            self.connection.close()  # disconnect if failure
             # return False
         else:
             self.receive_flag = True
@@ -62,7 +68,8 @@ class proxy_remote_player(Player):
     def end_game(self):
         response = ""
         try:
-            self.connection.sendall('["end-game"]'.encode())
+            message = json.dumps(["end-game"]).encode()
+            self.connection.sendall(message)
             response = self.connection.recv(recv_size_player)
             print("proxy end game 66", response)
             if response:
